26da475338ac78fa887f9f32362cef80cebec2ab
MabelMoncy <184001565+MabelMoncy@users.noreply.github.com>
2026-01-10 11:46:41 +0530

    updated for netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,21 +1,17 @@
-require('dotenv').config();
 const express = require('express');
 const { google } = require('googleapis');
 const cors = require('cors');
-const compression = require('compression');
-const helmet = require('helmet');
 const serverless = require('serverless-http');
+const helmet = require('helmet');
 
 const app = express();
 
-app.use(compression());
-// FIX FOR PDF PREVIEW: We disable the headers that block iframes
+app.use(cors());
+// Helmet config allows the PDF to be displayed in your iframe
 app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false
 }));
-app.use(cors());
-app.use(express.json());
 
 const ROOT_FOLDER_ID = '1bB6-3-q62cn2mfRZ9pfMl72M75_yZMp1';
 
@@ -70,7 +66,9 @@ app.get('/api/view/:fileId', async (req, res) => {
         const fileId = req.params.fileId;
         const meta = await drive.files.get({ fileId, fields: 'name, size' });
         
-        if (parseInt(meta.data.size) > 9437184) { 
+        // NETLIFY FIX: If file is > 5MB, proxying will likely fail/timeout.
+        // We redirect to the high-speed Google Drive preview instead.
+        if (parseInt(meta.data.size) > 5242880) { 
             return res.redirect(`https://drive.google.com/file/d/${fileId}/view`);
         }
 
@@ -90,26 +88,4 @@ app.get('/api/download/:fileId', async (req, res) => {
     } catch (e) { res.status(500).send('Error'); }
 });
 
-// Search
-app.get('/api/search', async (req, res) => {
-    try {
-        const { q } = req.query;
-        const drive = initDriveClient();
-        const response = await drive.files.list({
-            q: `name contains '${q.replace(/'/g, "\\'")}' and mimeType = 'application/pdf' and trashed = false`,
-            fields: 'files(id, name, size)',
-            pageSize: 10
-        });
-        const files = response.data.files.map(f => ({
-            id: f.id,
-            name: f.name,
-            size: (parseInt(f.size) / 1024 / 1024).toFixed(1) + ' MB',
-            viewUrl: `/api/view/${f.id}`,
-            downloadUrl: `/api/download/${f.id}`
-        }));
-        res.json({ success: true, data: files });
-    } catch (error) { res.status(500).json({ success: false }); }
-});
-
-module.exports = app;
 module.exports.handler = serverless(app);
